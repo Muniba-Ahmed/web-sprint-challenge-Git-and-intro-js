bba6710194ac04b08b7a1c66ee6c35a11cfe41b9
muniba <muniba@Munibas-MBP.lan>
2022-01-14 16:24:47 -0800

    finished and just checking

--- index.js
+++ index.js
@@ -243,12 +243,12 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(artists) {
+function get20s(array) {
   const lives = [];
-  for (let i = 0; i < artists.length; i++) {
-    if (artists[i].years.split("-") < "2000") {
-      if (artists[i].years.split("-") > "1900") {
-        lives.push(artists[i].name);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].years.split("-") < "2000") {
+      if (array[i].years.split("-") > "1900") {
+        lives.push(array[i].name);
       }
     }
   }
